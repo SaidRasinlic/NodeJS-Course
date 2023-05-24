b570c9f9d1556a80a5b492a5cac0a74ac0171aa0
Said Rasinlic <saidrasinlic.jr@gmail.com>
2023-05-24 14:53:25 +0200

    Add custom made middleware

---
+++ server/middleware/logEvents.js
@@ -0,0 +1,25 @@
+const { v4: uuid } = require('uuid');
+const { format } = require('date-fns');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = async (message, logName) => {
+  const logItem = `${format(new Date(), 'dd-MM-yyyy\tHH:mm:ss')}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
+    await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logEvents, logger };
